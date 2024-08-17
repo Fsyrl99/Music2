79657e5fc876628851001d7aa2cf9bb6a897a0f7
antoo69 <antobarry69@gmail.com>
2024-08-18 00:23:18 +0700

    Update play.py

--- Spotify_Music/utils/decorators/play.py
+++ Spotify_Music/utils/decorators/play.py
@@ -21,7 +21,9 @@ from Spotify_Music.utils.database import (
     is_maintenance,
 )
 from Spotify_Music.utils.inline import botplaylist_markup
-from config import PLAYLIST_IMG_URL, SUPPORT_CHAT, adminlist
+from config import PLAYLIST_IMG_URL, PRIVATE_BOT_MODE, MUST_JOIN
+from config import SUPPORT_GROUP as SUPPORT_CHAT
+from config import adminlist
 from strings import get_string
 
 links = {}
@@ -44,6 +46,17 @@ def PlayWrapper(command):
                 ]
             )
             return await message.reply_text(_["general_3"], reply_markup=upl)
+        if MUST_JOIN:
+            try:
+                await app.get_chat_member(MUST_JOIN, message.from_user.id)
+            except UserNotParticipant:
+                sub = await app.export_chat_invite_link(MUST_JOIN)
+                kontol = InlineKeyboardMarkup(
+                    [
+                        [InlineKeyboardButton("📑 Gabung Dulu", url=sub)]
+                    ]
+                )
+                return await message.reply_text(_["force_sub"].format(message.from_user.mention), reply_markup=kontol)    
 
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
